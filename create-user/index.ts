e2ca2f7b34764245e99bb435248ad21124f651bc
calmdowngirl <116081897+calmdowngirl@users.noreply.github.com>
2023-04-24 17:01:58 +0930

    wip

--- create-user/index.ts
+++ create-user/index.ts
@@ -1,14 +1,20 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'
 import { DynamoDB } from '@aws-sdk/client-dynamodb'
 import * as bcrypt from 'bcryptjs'
+import * as jwt from 'jsonwebtoken'
+import { readFileSync } from 'fs'
+
+interface TokenPayload {
+  email: string
+}
 
 const region = 'ap-southeast-2'
 const dynamo = new DynamoDB({ region })
 
 exports.handler = async (event: APIGatewayProxyEvent) => {
   const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    statusCode: 403,
+    body: JSON.stringify('Unauthorised'),
     headers: {
       'Content-Type': 'application/json',
     },
@@ -17,6 +23,25 @@ exports.handler = async (event: APIGatewayProxyEvent) => {
   try {
     if (event.httpMethod === 'PUT' && event.path === '/create-user') {
       const input = JSON.parse(event.body ?? '{}')
+      let secret: string = readFileSync('./secret', 'utf-8')
+
+      const token = event.headers?.['token']
+      if (!token || !secret) return response
+
+      let decodedToken
+      try {
+        decodedToken = jwt.verify(token, secret) as TokenPayload
+      } catch (e) {
+        console.log(e)
+        return response
+      }
+
+      await dynamo.getItem({
+        TableName: 'user',
+        Key: {
+          email: { S: decodedToken.email },
+        },
+      })
 
       // todo: isAuthorised
       // todo: data validation check
@@ -24,7 +49,7 @@ exports.handler = async (event: APIGatewayProxyEvent) => {
       const password = input.password
       const salt = await bcrypt.genSalt(5)
       const hashedPwd = await bcrypt.hash(password, salt)
-      console.log(salt, hashedPwd)
+
       await dynamo.putItem({
         TableName: 'user',
         Item: {
@@ -40,14 +65,17 @@ exports.handler = async (event: APIGatewayProxyEvent) => {
 
       response.body = JSON.stringify('Ok')
     } else {
-      ;(response.statusCode = 400),
-        (response.body = JSON.stringify('Bad Request'))
+      response.statusCode = 400
+      response.body = JSON.stringify('Bad Request')
     }
   } catch (e) {
     console.log(e)
-    ;(response.statusCode = 500),
-      (response.body = JSON.stringify('Server Error'))
+    response.statusCode = 500
+    response.body = JSON.stringify('Server Error')
   }
+
+  response.statusCode = 200
+  response.body = JSON.stringify('Ok')
 
   return response
 }
