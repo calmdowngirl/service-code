b232b5efdc9bac2f655ff5739b34924e42790903
calmdowngirl <116081897+calmdowngirl@users.noreply.github.com>
2023-04-28 17:05:27 +0930

    wip: data validation

--- create-user/index.ts
+++ create-user/index.ts
@@ -14,6 +14,18 @@ const region = 'ap-southeast-2'
 const dynamo = new DynamoDB({ region })
 const allowedRoles = ['su', 'admin']
 const allowedInputRoles = ['admin', 'consumer']
+const PATTERNS = {
+  nameAllowSpaceAndDash: /^[\w]+([\s-]?\w)+$/,
+  nameNoSpaceOrDash: /^[\w]*$/,
+  digits: /^\d*$/,
+  rationalNumber: /^[+-]?(\d+[.])?\d+$/,
+  phone: /^[+]?\d{3,15}$/,
+  noExecutable: /^[^`'"<>]+$/,
+  email:
+    // eslint-disable-next-line no-useless-escape
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  resetCode: /[a-zA-Z0-9]{6}/,
+}
 
 export const handler = async (event: APIGatewayProxyEvent) => {
   if (
@@ -24,10 +36,18 @@ export const handler = async (event: APIGatewayProxyEvent) => {
     return toReturn(400)
 
   try {
-    const input = JSON.parse(event.body)
+    // const input = JSON.parse(event.body)
+    const { email, role, name, password, phone, exp_at } = JSON.parse(
+      event.body
+    )
 
     /// todo
     //- [] data validation check
+    if (!allowedInputRoles.includes(role) || !PATTERNS.email.test(email))
+      return toReturn(403)
+    if (name && !PATTERNS.nameAllowSpaceAndDash.test(name)) return toReturn(403)
+    if (phone && !PATTERNS.phone.test(phone)) return toReturn(403)
+    // if (exp_at)
 
     const secret = readFileSync('./shared/secret', 'utf-8')
 
@@ -58,29 +78,25 @@ export const handler = async (event: APIGatewayProxyEvent) => {
 
     if (item.access_token.S !== (decodedToken as TokenPayload).token)
       return toReturn(403)
-    if (input.role === 'su') return toReturn(403)
-    if (item.role.S === input.role || item.role.S === 'consumer')
-      return toReturn(403)
-    if (item.role.S === 'admin' && input.role !== 'consumer')
-      return toReturn(403)
+    if (role === 'su') return toReturn(403)
+    if (item.role.S === role || item.role.S === 'consumer') return toReturn(403)
+    if (item.role.S === 'admin' && role !== 'consumer') return toReturn(403)
 
-    const password = input.password
-    const salt = await bcrypt.genSalt(1)
-    const hashedPwd = await bcrypt.hash(password, salt)
+    const putItem = { TableName: 'user', Item: {} as any }
+    putItem.Item.email = { S: email }
+    putItem.Item.sort_key = { S: email }
+    putItem.Item.role = { S: role }
+    putItem.Item.created_at = { N: Date.now().toString() }
+    if (name) putItem.Item.name = { S: name }
+    if (password) {
+      const salt = await bcrypt.genSalt(1)
+      const hashedPwd = await bcrypt.hash(password, salt)
+      putItem.Item.password = { S: hashedPwd }
+      putItem.Item.salt = { S: salt }
+    }
+    if (exp_at) putItem.Item.name = { N: exp_at }
 
-    await dynamo.putItem({
-      TableName: 'user',
-      Item: {
-        name: { S: input.name },
-        email: { S: input.email },
-        sort_key: { S: input.email },
-        password: { S: hashedPwd },
-        salt: { S: salt },
-        role: { S: input.role },
-        created_at: { N: Date.now().toString() },
-        exp_at: { N: '-1' },
-      },
-    })
+    await dynamo.putItem(putItem)
 
     return toReturn(200)
   } catch (e) {
